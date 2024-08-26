57aaf4a1edacda6cc757046f8ff796dea053c2f5
Your Name <your_email@example.com>
2024-08-26 00:54:41 -1100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// var num=-88;
+// var result=Math.abs(num);
+// console.log(result)
+
+
+var number=-66;
+console.log(Math.abs(number))
+
+// var num1=7.7;
+// console.log(
+//     Math.ceil(num1)
+// )
+
+var num=7.00001;
+console.log(
+    Math.ceil(num)
+)
+
+var num1=7.7;
+console.log(
+    Math.floor(num1),"<br><br>"
+)
+
+var num=3.5;
+console.log(Math.round(num))
